e41fefdc203a74624b8b1c174393b655beb56817
dhinahh <dhinahaulia@gmail.com>
2022-12-06 21:29:09 +0700

    Dhinah Aulia S (H1D020057) - shift G

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+ {
+ path: 'home',
+ loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+ canLoad: [AuthGuard] // Secure all child pages
+ },
+ {
+ path: '',
+ redirectTo: 'login',
+ pathMatch: 'full'
+ },
+ {
+ path: 'login',
+ loadChildren: () => import('./login/login.module').then(m =>
+ m.LoginPageModule),
+ canLoad: [AutoLoginGuard]
+ },
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+ imports: [
+ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+ ],
+ exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
